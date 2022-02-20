70c5de864d7cdf425401d4da4e508ab262956af4
RobuxTRex <robuxtrex@gmail.com>
2022-02-20 16:55:09 +0100

    funny changes

--- components/screens/ErrorScreen.js
+++ components/screens/ErrorScreen.js
@@ -6,7 +6,9 @@ export default function ErrorScreen({ error }) {
       <h1>An error occurred</h1>
       <p>The system will automatically restart in 5 seconds</p>
 
-      
+      <div>
+        <div>Error Information</div>
+      </div>
     </div>
   )
 }
