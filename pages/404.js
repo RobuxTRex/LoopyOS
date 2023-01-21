f092555b29ea28f6ca2204c89bb1ad8d449cb13d
Sulphur <robuxtrex@gmail.com>
2023-01-21 19:32:32 +0000

    Fixed httpEquiv issue.

--- pages/404.js
+++ pages/404.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <Head>
         <title>LoopyOS - 404</title>
-        <meta http-equiv="Refresh" content="0; url='/LoopyOS'" />
+        <meta httpEquiv="Refresh" content="0; url='/LoopyOS'" />
     </Head>
   )
 }
